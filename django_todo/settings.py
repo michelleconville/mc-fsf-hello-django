facc86df2f56b84836d52d99f5f8f920b57987b1
michelleconville <michelle.conville@gmail.com>
2023-04-23 16:12:00 +0000

    Add files to github

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+# import env
+
 if os.path.isfile("env.py"):
     import env
 
@@ -73,19 +76,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_todo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+# if "DATABASE_URL" in os.environ:
+#     DATABASES = {
+#         'default': os.environ.get("DATABASE_URL")
 #     }
-# }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 DATABASES = {
-   'default': os.environ.get("DATABASE_URL")
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
